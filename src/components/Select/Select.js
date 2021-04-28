ba90c5ba506487fc8db2c242a434bfb2f64950df
tannerbaum <hoisington.tanner@gmail.com>
2021-04-28 22:41:37 +0200

    initial select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,68 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  appearance: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const SelectOverlay = styled.div`
+  /* To click through the padding  */
+  pointer-events: none;
+
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 52px 12px 16px;
+  border: none;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + &{
+    /* ? Unsure how I am supposed to get the browser default color here, firefox claims initial is invalid */
+    outline: 2px solid;
+  }
+
+  ${NativeSelect}:hover + &{
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: ${({size}) => size + "px"};
+  height: ${({size}) => size + "px"};;
+
+  margin: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+  const size = 24;
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <SelectOverlay>
+        {displayedValue}
+        <IconWrapper size={size}>
+          <Icon id="chevron-down" size={size} />
+        </IconWrapper>
+      </SelectOverlay>
+    </Wrapper>
   );
 };
 
