a419f275dc49ae5aecc2bef63735e1f9e74b5cb4
tannerbaum <hoisington.tanner@gmail.com>
2021-04-28 14:35:50 +0200

    initial progress bar solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,49 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--padding": "none"
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": "none"
+  },
+  large: {
+    "--height": "24px",
+    "--padding": "4px"
+  }
+};
+
+const Bar = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-sizing: border-box;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px; 
+  
+  width: 370px;
+  height: var(--height);
+  padding: var(--padding);
+  
+  position: relative;
+  overflow: hidden;
+
+  &:after{
+    content: '';
+    display: block;
+    background-color: ${COLORS.primary};
+    
+    height: 100%;
+    width: ${({value}) => value + "%"};
+    border-radius: ${({value}) => value === 100 ? "4px" : "4px 0px 0px 4px;"};
+  }
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  return <Bar style={styles} value={value} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100"><VisuallyHidden>{value}</VisuallyHidden></Bar>;
 };
 
 export default ProgressBar;
