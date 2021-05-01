55b99a3147e0d9ddfd9cccd466c463b4da0c40c6
tannerbaum <hoisington.tanner@gmail.com>
2021-05-01 16:15:22 +0200

    initial input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,57 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const BaseInput = styled.input`
+  appearance: none;
+  border: none; 
+  width: 100%;
+  padding-bottom: 5px;
+  
+  color: ${COLORS.gray700};
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+  }
+
+  &:focus{
+    outline-offset: 2px;
+    border-radius: 2px;
+  }
+
+  &:hover{
+    font-weight: 700;
+
+    &::placeholder {
+    font-weight: 400;
+  }
+  }
+`;
+const SmallInput = styled(BaseInput)`
+  font-size: 14px;
+  border-bottom: 1px solid #000000;
+  padding-left: 25px;
+`;
+const LargeInput = styled(BaseInput)`
+  font-size: 18px;
+  border-bottom: 2px solid #000000;
+  padding-left: 40px;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: ${({width}) => width + 'px'};
+
+  &:hover svg {
+    stroke-width: 2px; 
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 2px;
+  left: 2px;
+`
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +64,26 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  let Component;
+  if (size === "small") {
+    Component = SmallInput;
+  } else if (size === "large") {
+    Component = LargeInput;
+  } else {
+    throw new Error(`Unrecognized Input size: ${size}`);
+  }
+
+  return <>
+    <VisuallyHidden>
+      <label>{label}</label>
+    </VisuallyHidden>
+    <InputWrapper width={width}>
+      <IconWrapper>
+        <Icon id={icon} size={size === "large" ? 20 : 14}   />
+      </IconWrapper>
+      <Component placeholder={placeholder} />
+    </InputWrapper>
+    </>;
 };
 
 export default IconInput;
